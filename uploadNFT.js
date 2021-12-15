3f1c723ce12c600e62e491a094000764bf9b86ee
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2021-12-16 03:53:03 +0700

    add Display Name, Description for metadata

--- uploadNFT.js
+++ uploadNFT.js
@@ -1,19 +1,25 @@
 // call the config method that will read our environmental variables file and save them
-require('dotenv').config();
-const axios = require('axios');
-const chalk = require('chalk');
-const fs = require('fs');
-const imageToBase64 = require('image-to-base64');
+require("dotenv").config();
+const axios = require("axios");
+const chalk = require("chalk");
+const fs = require("fs");
+const imageToBase64 = require("image-to-base64");
 
 let unsortedFiles = [];
 let filesToUpload = [];
+
+const _AMOUNT = 5
+const _STOP = _AMOUNT - 1
+const ITEM_NAME = 'The Lightning #'
+const ITEM_DESCRIPTION = 'BUY ME NOW!!!!'
+
 
 //* ************************************************** //
 //* *************** Helper Functions ***************** //
 //* ************************************************** //
 function compareFileNames(f1, f2) {
-  let name1 = f1.split('.')[0];
-  let name2 = f2.split('.')[0];
+  let name1 = f1.split(".")[0];
+  let name2 = f2.split(".")[0];
   return name1 === name2;
 }
 
@@ -25,7 +31,7 @@ function compareFileNames(f1, f2) {
 // Now since this takes a callback add the resolve and reject to it
 const getFiles = () => {
   return new Promise((resolve, reject) => {
-    fs.readdir('./racecars/', (err, files) => {
+    fs.readdir("./racecars/", (err, files) => {
       if (err) {
         reject(err);
       }
@@ -35,7 +41,7 @@ const getFiles = () => {
       resolve(true);
     });
   });
-}
+};
 
 // Loops through the "unsortedFiles" array and compares the file names
 // If there is a match they get put inside their own array
@@ -47,54 +53,59 @@ const modifyFilesForUpload = async () => {
       return;
     }
     if (compareFileNames(element1, element2)) {
-      await imageToBase64('./racecars/' + element2) // path to the image
+      await imageToBase64("./racecars/" + element2) // path to the image
         .then((response) => {
           filesToUpload.push({
-            fileReference: element2.split('.')[0],
-            meta: fs.readFileSync('./racecars/' + element1, {
-              encoding: 'utf8',
-              flag: 'r'
+            fileReference: element2.split(".")[0],
+            meta: fs.readFileSync("./racecars/" + element1, {
+              encoding: "utf8",
+              flag: "r",
             }),
             img: response,
           });
         })
         .catch((error) => {
-          console.log(chalk.yellow.bold(`Error in imageToBase64 ${error} .............`));
+          console.log(
+            chalk.yellow.bold(`Error in imageToBase64 ${error} .............`)
+          );
         });
     }
   }
-}
+};
 
 async function uploadMyNFT(metadata, assetNumber, base64ImageData) {
+  let newAssetName = `BETA${assetNumber}`
   const body = {
-    AssetName: assetNumber,
+    AssetName: newAssetName,
     previewImageNft: {
-      mimetype: 'image/png',
+      mimetype: "image/png",
+      displayName: ITEM_NAME + assetNumber,
       fileFromBase64: base64ImageData,
+      description: ITEM_DESCRIPTION,
       metadataPlaceHolder: [
         {
-          name: 'car',
-          value: metadata.car,
+          name: "edition",
+          value: metadata.edition,
         },
         {
-          name: 'background',
-          value: metadata.background,
+          name: "Background",
+          value: metadata.Background,
         },
         {
-          name: 'helmet',
-          value: metadata.helmet,
+          name: "Wheels",
+          value: metadata.Wheels,
         },
         {
-          name: 'driverName',
-          value: metadata.driverName,
+          name: "Color",
+          value: metadata.Color,
         },
         {
-          name: 'sexuality',
-          value: metadata.sexuality,
+          name: "Glass",
+          value: metadata.Glass,
         },
         {
-          name: 'topSpeed',
-          value: metadata.topSpeed,
+          name: "Sticker",
+          value: metadata.Sticker,
         },
       ],
     },
@@ -102,26 +113,28 @@ async function uploadMyNFT(metadata, assetNumber, base64ImageData) {
 
   const data = JSON.stringify(body);
 
-
   const config = {
-    method: 'post',
+    method: "post",
     url: `https://api.nft-maker.io/UploadNft/${process.env.API_KEY}/${process.env.PROJECT_ID}`,
     headers: {
-      accept: 'text/plain',
-      'Content-Type': 'application/json'
+      accept: "text/plain",
+      "Content-Type": "application/json",
     },
     data: data,
   };
 
   await axios(config)
     .then(function (response) {
-      console.log(chalk.blueBright.bgBlackBright.bold(`🚀 Success ${assetNumber} has been uploaded`));
+      console.log(
+        chalk.blueBright.bgBlackBright.bold(
+          `🚀 Success ${assetNumber} has been uploaded`
+        )
+      );
     })
     .catch(function (error) {
       console.log(chalk.yellow.bold(`Whoops!!! ${error} ............... 🖕🏿`));
     });
 }
-
 
 //* ************************************************** //
 //* ***************** Run the script ***************** //
@@ -134,23 +147,26 @@ async function* asyncGenerator() {
   let i = 0; // //The asset number to start on
   yield getFiles();
   yield modifyFilesForUpload();
-  while (i < 5) {
+  while (i < _AMOUNT) {
     const metadata = JSON.parse(filesToUpload[i].meta);
-    const assetNumber = filesToUpload[i].fileReference.padStart(5, '0');
+    const assetNumber = filesToUpload[i].fileReference.padStart(3, "0"); // (5, "0")
     const base64ImageData = filesToUpload[i].img;
     yield uploadMyNFT(metadata, assetNumber, base64ImageData);
     yield i++;
   }
 }
 
-
 // The for await...of statement creates a loop iterating over async iterable objects
 // In order to use the generator we need to run the generator function which will return a generator object that allows use to use the generator
 (async function () {
   // As the generator is asynchronous, we can use await inside it,
   for await (let num of asyncGenerator()) {
-    if (num === 4) {
-      console.log(chalk.magentaBright.bold('Finished generating images, now Axios will upload the shit. lmao...'));
+    if (num === _STOP) {
+      console.log(
+        chalk.magentaBright.bold(
+          "Finished generating images, now Axios will upload the shit. lmao..."
+        )
+      );
       process.exitCode = 0;
     }
   }
